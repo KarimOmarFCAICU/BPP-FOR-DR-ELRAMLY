22d1daf045572871524e6b8ca3306ee37a59e824
Karim Omar <omarkareem409@gmail.com>
2025-09-26 21:08:08 +0300

    اخر تعديل بعد الفلترين

--- main/CS213-2025-2026-Assignemnt1-LibraryAndExample/main/main.cpp
+++ main/CS213-2025-2026-Assignemnt1-LibraryAndExample/main/main.cpp
@@ -3,43 +3,105 @@
 #include "Image_Class.h"
 
 using namespace std;
+void Revert(Image&img){
+    for(int i = 0; i < img.width ; i++){
+        for(int j = 0; j < img.height; j++){
+            for(int k =0; k < 3; k++){
+                img(i,j,k) = 255 -img(i,j,k);
+            }
+        }
+    }
+
+}
+void AddingFrame(Image&img, char color){
+    int frame_size = min(img.width,img.height) *0.05;
+    Image framed_pic(img.width +2*frame_size , img.height +2*frame_size);
+    for(int i = 0; i < framed_pic.width ; i++){
+        for(int j = 0; j < framed_pic.height; j++){
+                if(i < frame_size || j < frame_size || i >= frame_size + img.width || j >= frame_size + img.height){
+                     if(color == 'W'){
+                        framed_pic(j, i, 0) = 255;
+                        framed_pic(j, i, 1) = 255;
+                        framed_pic(j, i, 2) = 255;
+                     }
+                     else if (color == 'B'){
+                        framed_pic(j, i, 0) = 0;
+                        framed_pic(j, i, 1) = 0;
+                        framed_pic(j, i, 2) = 0;
+                     }
+
+                    else if (color == 'R'){
+                       framed_pic(j, i, 0) = 255;
+                       framed_pic(j, i, 1) = 0;
+                       framed_pic(j, i, 2) = 0;
+                    }
+                    else if (color == 'U'){
+                       framed_pic(j, i, 0) = 0;
+                       framed_pic(j, i, 1) = 0;
+                       framed_pic(j, i, 2) = 255;
+                    }
+                    else if (color == 'G'){
+                        framed_pic(j, i, 0) = 0;
+                        framed_pic(j, i, 1) = 255;
+                        framed_pic(j, i, 2) = 0;
+
+                    }
+                    }
+
+                else {
+                    for(int k =0; k <3; k++){
+                        framed_pic(j,i,k) = img(j-frame_size , i-frame_size , k);
+                    }
+                }
+        }
+    }
+    img = framed_pic;
+
+}
+void PrintTheList(){
+    cout << "WELCOME IN BABY PS APP ENTER THE NAME OF NUM OF THE FILTER" << endl;
+    cout << "1. \n";
+    cout << "2. \n";
+    cout << "3.Revert the image \n";
+    cout << "4. \n";
+    cout << "5. \n";
+    cout << "6. \n";
+    cout << "7. \n";
+    cout << "8. \n";
+    cout << "9.Adding frame \n";
+    cout << "10. \n";
+    cout << "11. \n";
+    cout << "12. \n";
+
+
+}
 
 int main()
 {
-    Image image;
-
-    string image_filename = "arrow.jpg";
-
-    bool loaded = image.loadNewImage(image_filename);
-
-    if (loaded) {
-        cout << "Image loaded successfully from: " << image_filename << endl;
-        cout << "Image Dimensions: Width = " << image.width << ", Height = " << image.height << endl;
-        cout << "Number of channels: " << image.channels << endl;
-    } else {
-        cout << "Program will now exit due to image loading failure." << endl;
-        return 1;
+    string filename , output_filename;
+    cout << "Enter your image name *with extension* : \n";
+    cin >> filename;
+    Image img(filename);
+    PrintTheList();
+    int num;
+    cin >> num;
+    if(num == 3){
+        Revert(img);
     }
-
-    if (image.width > 0 && image.height > 0) {
-        unsigned char red = image.getPixel(0, 0, 0);
-        unsigned char green = image.getPixel(0, 0, 1);
-        unsigned char blue = image.getPixel(0, 0, 2);
-        cout << "Pixel (0,0) - R: " << (int)red << ", G: " << (int)green << ", B: " << (int)blue << endl;
-
-        image.setPixel(10, 10, 0, 255);
-        image.setPixel(10, 10, 1, 0);
-        image.setPixel(10, 10, 2, 0);
+    else if (num == 9) {
+        char color;
+        cout << "\nPlease choose a color for the frame:\n";
+        cout << "Enter 'W' for White\n";
+        cout << "Enter 'B' for Black\n";
+        cout << "Enter 'R' for Red\n";
+        cout << "Enter 'G' for Green\n";
+        cout << "Enter 'U' for Blue\n";
+        cout << "Your choice: ";
+        cin >> color;
+        AddingFrame(img , color);
     }
-
-    string output_filename = "output_test.jpg";
-    bool saved = image.saveImage(output_filename);
-
-    if (saved) {
-        cout << "Image saved successfully as: " << output_filename << endl;
-    } else {
-        cout << "Failed to save image: " << output_filename << endl;
-    }
-
-    return 0;
+    cout << "\nEnter the desired name for the new image (e.g., inverted_image.png).\n";
+    cout << "Supported extensions are: .jpg, .jpeg, .png, .bmp, .tga: ";
+    cin >> output_filename;
+    img.saveImage(output_filename);
 }
